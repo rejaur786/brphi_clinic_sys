6bae153db599afda883a732a63958c61511540d2
rrahman <rrahman@firstderivatives.com>
2021-06-04 00:37:42 +0100

    BRPHI-017 - Patient DashBoard Initial Commit

--- src/components/PatientList.js
+++ src/components/PatientList.js
@@ -20,19 +20,19 @@ export default class PatientList extends Component {
     componentDidMount(){
         //axios.get("http://localhost:8081/rest/books")
             //.then(response => console.log(response.data));
-         //axios.get("http://localhost:8081/rest/books")
-         //.then(response => response.data)
-         //.then((data) => {
-            //this.setState({patients: data});
-         //});
+         axios.get("https://ji224k9a68.execute-api.ap-south-1.amazonaws.com/dev/gramgp/patient/emrid_1")
+         .then(response => response.data)
+         .then((data) => {
+            this.setState({patients: data.lastLowLevelResult.queryResult.items});
+         });
 
 
         var data;
-        data = [{"time":"Fri Dec 16 2020 09:15", "patientName":"Amjad Khan", "emrNo":"1234", "dob":"17-09-1988", "reason":"I can't sleep"},
+       /* data = [{"time":"Fri Dec 16 2020 09:15", "patientName":"Amjad Khan", "emrNo":"1234", "dob":"17-09-1988", "reason":"I can't sleep"},
                    {"time":"Fri Dec 16 2020 09:30", "patientName":"Bilal Khan", "emrNo":"786", "dob":"10-09-1978", "reason":"High Blood Pressure"},
                    {"time":"Fri Dec 16 2020 09:45", "patientName":"Nasima Begum", "emrNo":"987", "dob":"23-07-1997", "reason":"Diabetes"}]
 
-        this.setState({patients: data});
+        this.setState({patients: data});*/
 
     }
 
@@ -50,54 +50,39 @@ export default class PatientList extends Component {
                               <th>Time</th>
                               <th>Patient Name</th>
                               <th>EMR No.</th>
+                              <th>Gender</th>
                               <th>Date Of Birth</th>
                               <th>Reason</th>
+                              <th></th>
                             </tr>
                           </thead>
                           <tbody>
-                            {/*
-                                this.state.books.length === 0 ?
-                                 <tr align="center">
-                                      <td colSpan="6">{this.state.books.length} Books Available.</td>
-                                 </tr> :
-                                 this.state.books.map((book) => (
-                                 <tr key={book.id}>
-                                     <td>
-                                            <Image src={book.coverPhotoURL} roundedCircle width="25" height="25" /> {book.title}
-                                     </td>
-                                     <td>{book.author}</td>
-                                     <td>{book.isbnNumber}</td>
-                                     <td>{book.price}</td>
-                                     <td>{book.language}</td>
-                                     <td>
-                                        <ButtonGroup>
-                                            <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{''}
-                                            <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-                                        </ButtonGroup>
+                           {
+                               this.state.patients.length === 0 ?
+                                   <tr align="center">
+                                      <td colSpan="6">{this.state.patients.length} Patients Available.</td>
+                                   </tr> :
 
-                                     </td>
-                                 </tr>
+                               this.state.patients.map((patient) => (
+                                   <tr key={patient.PK.s}>
+                                      <td>
+                                          {patient.lastAppointmentDateTime.s}
+                                      </td>
+                                      <td>{patient.firstname.s} {patient.lastname.s}</td>
+                                      <td>{patient.PK.s}</td>
+                                      <td>{patient.gender.s}</td>
+                                      <td>{patient.dob.s}</td>
+                                      <td>{patient.reason.s}</td>
+                                      <td>
+                                          <ButtonGroup>
+                                             <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{''}
+                                             <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
+                                          </ButtonGroup>
+                                      </td>
+                                   </tr>
                                  ))
-                            */}
+                           }
 
-                            {this.state.patients.map((patient) => (
-                                                             <tr key={patient.emrNo}>
-                                                                 <td>
-                                                                       {patient.time}
-                                                                 </td>
-                                                                 <td>{patient.patientName}</td>
-                                                                 <td>{patient.emrNo}</td>
-                                                                 <td>{patient.dob}</td>
-                                                                 <td>{patient.reason}</td>
-                                                                 <td>
-                                                                    <ButtonGroup>
-                                                                        <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>{''}
-                                                                        <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-                                                                    </ButtonGroup>
-
-                                                                 </td>
-                                                             </tr>
-                                                             ))}
                           </tbody>
                     </Table>
                 </Card.Body>
